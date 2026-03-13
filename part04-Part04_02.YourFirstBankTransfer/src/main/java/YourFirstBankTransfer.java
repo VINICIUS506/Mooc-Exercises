fbd274c0a16e91f3369ac2dbabe2175d75fd5e86
VINICIUS506 <viny.fsm@gmail.com>
2026-03-13 21:16:23 +0000

    Updated Exercises

--- part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
+++ part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
@@ -4,5 +4,14 @@ public class YourFirstBankTransfer {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // write your program here
+
+        Account MatthewsAccount = new Account ("Matthews account", 1000.0);
+        Account myAccount = new Account ("My account", 0.0);
+
+        MatthewsAccount.withdrawal(100.00);
+        myAccount.deposit(100.0);
+
+        System.out.println(MatthewsAccount);
+        System.out.println(myAccount);
     }
 }
